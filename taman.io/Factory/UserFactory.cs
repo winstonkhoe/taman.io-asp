62500841426223543613851ae70a3216147aa67a
winstonkhoe <winstonkhoe@yahoo.com>
2022-06-05 17:10:23 +0700

    Added Database, Singleton Database, Entities Model

---
+++ taman.io/Factory/UserFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace taman.io.Factory
+{
+    public class UserFactory
+    {
+        public static user CreateEmployee(string email, string password, string name, DateTime dob, string gender, string phone, string address, int salary)
+        {
+            return new user
+            {
+                EmployeeAddress = address,
+                EmployeeDOB = dob,
+                EmployeeEmail = email,
+                EmployeeGender = gender,
+                EmployeeName = name,
+                EmployeePassword = password,
+                EmployeePhone = phone,
+                EmployeeSalary = salary
+            };
+
+        }
+    }
+}
---
+++ taman.io/Repository/DatabaseSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using taman.io.Model;
+
+namespace taman.io.Repository
+{
+    public class DatabaseSingleton
+    {
+        public static TamanEntities db;
+
+        public static TamanEntities Connections()
+        {
+            return (db == null) ? new TamanEntities() : db;
+        }
+    }
+}
